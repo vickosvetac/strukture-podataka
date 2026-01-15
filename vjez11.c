efa4a7523280c88aeac438238a65b95744c2f861
vickosvetac <svetac123123@gmail.com>
2026-01-15 15:59:32 +0100

    Update vjez11.c

--- vjez11.c
+++ vjez11.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TABLE_SIZE 11
+
+//strukture
+
 typedef struct city* TreePos;
 typedef struct country* Position;
 
@@ -18,68 +22,108 @@ struct country {
     TreePos root;
 };
 
-//grad u stablo
+//funkcije
+
+int hashFunction(char* name);
+
+TreePos insertCity(TreePos root, char* name, int pop);
+int printCities(TreePos root);
+int searchCities(TreePos root, int min);
+int freeCityTree(TreePos root);
+
+Position createCountry(char* name);
+int insertCountrySorted(Position head, Position newCountry);
+int loadCities(Position country, char* filename);
+int freeCountries(Position head);
+
+//hash
+
+int hashFunction(char* name) {
+    int sum = 0;
+    for (int i = 0; i < 5 && name[i] != '\0'; i++)
+        sum += (int)name[i];
+
+    return sum % TABLE_SIZE;
+}
+
+//stablo gradova
+
 TreePos insertCity(TreePos root, char* name, int pop) {
     if (root == NULL) {
         TreePos new = malloc(sizeof(struct city));
+        if (!new) return NULL;
+
         strcpy(new->name, name);
         new->population = pop;
         new->left = new->right = NULL;
         return new;
     }
 
-    if (pop < root->population || 
-        (pop == root->population && strcmp(name, root->name) < 0)) {
+    if (pop < root->population ||
+        (pop == root->population && strcmp(name, root->name) < 0))
         root->left = insertCity(root->left, name, pop);
-    } else {
+    else
         root->right = insertCity(root->right, name, pop);
-    }
 
     return root;
 }
 
-//ispis gradova
 int printCities(TreePos root) {
-    if (root == NULL) return 0;
+    if (!root) return 0;
+
     printCities(root->left);
     printf("   %s (%d)\n", root->name, root->population);
     printCities(root->right);
+
     return 0;
 }
 
-//min stanovnici
 int searchCities(TreePos root, int min) {
-    if (root == NULL) return 0;
+    if (!root) return 0;
+
     searchCities(root->right, min);
-    if (root->population > min) {
+    if (root->population > min)
         printf("   %s (%d)\n", root->name, root->population);
-    }
     searchCities(root->left, min);
+
     return 0;
 }
 
-//novi cvor
+int freeCityTree(TreePos root) {
+    if (!root) return 0;
+
+    freeCityTree(root->left);
+    freeCityTree(root->right);
+    free(root);
+
+    return 0;
+}
+
+//drzave
+
 Position createCountry(char* name) {
     Position new = malloc(sizeof(struct country));
+    if (!new) return NULL;
+
     strcpy(new->name, name);
     new->root = NULL;
     new->next = NULL;
+
     return new;
 }
 
-//drzava u vezanu listu
-int insertCountrySorted(Position head, Position new) {
+int insertCountrySorted(Position head, Position newCountry) {
     Position p = head;
-    while (p->next != NULL && strcmp(p->next->name, new->name) < 0) {
-        p = p->next;
-    }
 
-    new->next = p->next;
-    p->next = new;
+    while (p->next && strcmp(p->next->name, newCountry->name) < 0)
+        p = p->next;
+
+    newCountry->next = p->next;
+    p->next = newCountry;
+
     return 0;
 }
 
-//ucitavanje gradova iz datoteke
 int loadCities(Position country, char* filename) {
     FILE* f = fopen(filename, "r");
     char city[50];
@@ -90,59 +134,61 @@ int loadCities(Position country, char* filename) {
         return -1;
     }
 
-    while (fscanf(f, " %[^,],%d", city, &pop) == 2) {
+    while (fscanf(f, " %[^,],%d", city, &pop) == 2)
         country->root = insertCity(country->root, city, pop);
-    }
 
     fclose(f);
     return 0;
 }
 
-/* ===== FREE FUNKCIJE ===== */
-
-//free stabla gradova
-void freeCityTree(TreePos root) {
-    if (root == NULL) return;
-    freeCityTree(root->left);
-    freeCityTree(root->right);
-    free(root);
-}
-
-//free svih drzava
-void freeCountries(Position head) {
+int freeCountries(Position head) {
     Position temp;
-    while (head->next != NULL) {
+
+    while (head->next) {
         temp = head->next;
         head->next = temp->next;
+
         freeCityTree(temp->root);
         free(temp);
     }
+
+    return 0;
 }
 
-// Glavni program
+
 int main() {
     FILE* f = fopen("drzave.txt", "r");
     char countryName[50], fileName[50];
 
-    struct country head = {.name = "", .next = NULL, .root = NULL};
+    Position hashTable[TABLE_SIZE];
 
-    //gradovi i drzave
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        hashTable[i] = malloc(sizeof(struct country));
+        hashTable[i]->next = NULL;
+        hashTable[i]->root = NULL;
+        strcpy(hashTable[i]->name, "");
+    }
+
     while (fscanf(f, "%s %s", countryName, fileName) == 2) {
+        int key = hashFunction(countryName);
+
         Position new = createCountry(countryName);
         loadCities(new, fileName);
-        insertCountrySorted(&head, new);
+        insertCountrySorted(hashTable[key], new);
     }
     fclose(f);
 
     //ispis
-    Position p = head.next;
-    while (p != NULL) {
-        printf("\nDrzava: %s\n", p->name);
-        printCities(p->root);
-        p = p->next;
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Position p = hashTable[i]->next;
+        while (p) {
+            printf("\nDrzava: %s (hash %d)\n", p->name, i);
+            printCities(p->root);
+            p = p->next;
+        }
     }
 
-    //pretraga prema min stanovnika
+    //search
     char searchCountry[50];
     int minPop;
 
@@ -151,18 +197,23 @@ int main() {
     printf("Minimalni broj stanovnika: ");
     scanf("%d", &minPop);
 
-    p = head.next;
-    while (p && strcmp(p->name, searchCountry) != 0) {
+    int key = hashFunction(searchCountry);
+    Position p = hashTable[key]->next;
+
+    while (p && strcmp(p->name, searchCountry))
         p = p->next;
-    }
 
     if (p)
         searchCities(p->root, minPop);
     else
         printf("Drzava ne postoji.\n");
 
-    // === FREE MEMORIJE ===
-    freeCountries(&head);
+    //free
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        freeCountries(hashTable[i]);
+        free(hashTable[i]);
+    }
 
     return 0;
 }
+
